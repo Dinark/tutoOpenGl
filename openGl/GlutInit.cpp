b8baafa646839052604fd55bb83706bed40da2ee
DK <DK>
2019-09-21 01:11:50 +0200

    mise en noir du fond ecran

--- openGl/GlutInit.cpp
+++ openGl/GlutInit.cpp
@@ -115,7 +115,7 @@ void Core::Init::GlutInit::displayCallback(void)
 		return;
 
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glClearColor(0.0, 0.0, 0.5, 1);
+	glClearColor(0.0, 0.0, 0.0, 1);
 	listener->notifyBeginFrame();
 	listener->notifyDisplayFrame();
 	glutSwapBuffers();
